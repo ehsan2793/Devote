9713bf0c93e4b097fe5785af71883cd74f87c8d6
ehsan2793 <ehsan2793@gmail.com>
2023-07-01 14:35:19 -0700

    organized

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Ehsan Rahimi on 7/1/23.
+//
+
+import Foundation
+
+// MARK: - FORMATER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: -  UI
+
+// MARK: - UX
